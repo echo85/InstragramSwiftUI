48f22aad36a0ad1a295e4384d6affa268eb2b9e8
Carlo Falchi <carlo.falchi@prada.com>
2021-03-26 17:42:50 +0100

    Initial Commit

---
+++ InstagramSwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Carlo Falchi on 26/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUITutorialApp.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by Carlo Falchi on 26/03/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
